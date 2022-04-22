91abd888c53dab41b5ab11d53d7ba3070c4640c7
mariarodina <q6m4?Qskk#>
2022-04-22 16:02:34 +0300

    дз 9. Обьекты и классы.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,19 @@
 package com.company;
 
+import com.company.homework9.Author;
+import com.company.homework9.Book;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Author author = new Author("Ivan", "Ivanov");
+        Book book = new Book("Book1", author, 2010);
+        System.out.println(book.getTitle() + " " + book.getAuthor().getFirstName() + " " + book.getAuthor().getLasttName()
+                + " " + book.getYear());
+
+        book.setYear(2020);
+        System.out.println(book.getTitle() + " " + book.getAuthor().getFirstName() + " " + book.getAuthor().getLasttName()
+                + " " + book.getYear());
+
     }
 }
---
+++ src/com/company/homework9/homework9.java
@@ -0,0 +1,4 @@
+package com.company.homework9;
+
+public class homework9 {
+}
